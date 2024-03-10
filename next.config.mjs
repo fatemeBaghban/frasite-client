4175273d4f76ead3ab3206d29d2ca539585fee2b
moeen dehqan <moeen.dehqan@gmail.com>
2024-03-10 14:53:30 +0330

    .

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['ipgrp.ir'], // اضافه کردن hostname به لیست تصاویر مجاز
+  },
 };
 
 export default nextConfig;
