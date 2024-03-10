18c24a2f9d2f5196f806f28c1c1d12929ad7f92a
Fateme baghban <fateme.baghban03@gmail.com>
2024-03-10 03:17:50 -0700

    branch

--- components/branch.js
+++ components/branch.js
@@ -1,4 +1,22 @@
-export default function SearchTable() {
+import { domin, onRun } from "@/pages/api/config";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Branches() {
+  const [data, setData] = useState([]);
+
+  const postData = () => {
+    axios
+      .post(onRun + "/branch/getup", { Domain: domin })
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  useEffect(postData, []);
+
   return (
     <>
       <div className="w-full h-full bg-white">
@@ -59,52 +77,53 @@ export default function SearchTable() {
                   <th scope="col" className="px-6 py-3"></th>
                 </tr>
               </thead>
-              <tbody  className="rounded-ful">
-                {Array.from({ length: 8 }).map((index) => (
-                  <tr
-                    className="bg-[#FBFBFB] border-b border-white rounded-full border-8 shadow-md mt-2"
-                    key={index.Code}
-                  >
-                    <th
-                      scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+              <tbody className="rounded-ful">
+                {data.map((index) => {
+                  console.log(index);
+
+                  return (
+                    <tr
+                      className="bg-[#FBFBFB] border-b border-white rounded-full border-8 shadow-md mt-2"
+                      key={index.Code}
                     >
-                      <p>{index.Province}</p>
-                    </th>
-                    <td className="px-6 py-4">{index.City} </td>
-                    <td className="px-6 py-4">{index.Types}</td>
-                    <td className="px-6 py-4">
-                      {index.Address}
-                    </td>
-                    <td className="px-6 py-4 text-right">
-                      <div className="flex flex-row">
-                        <svg
-                          viewBox="0 0 24 24"
-                          fill="currentColor"
-                          height="2em"
-                          width="2em"
-                        >
-                          <path d="M12 14c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2z" />
-                          <path d="M11.42 21.814a.998.998 0 001.16 0C12.884 21.599 20.029 16.44 20 10c0-4.411-3.589-8-8-8S4 5.589 4 9.995c-.029 6.445 7.116 11.604 7.42 11.819zM12 4c3.309 0 6 2.691 6 6.005.021 4.438-4.388 8.423-6 9.73-1.611-1.308-6.021-5.294-6-9.735 0-3.309 2.691-6 6-6z" />
-                        </svg>
-                        <a
-                          href="#"
-                          className="font-medium text-gray-600 hover:underline"
-                        >
+                      <th
+                        scope="row"
+                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                      >
+                        <p>{index.Province}</p>
+                      </th>
+                      <td className="px-6 py-4">{index.City} </td>
+                      <td className="px-6 py-4">{index.Types}</td>
+                      <td className="px-6 py-4">{index.Address}</td>
+                      <td className="px-6 py-4 text-right">
+                        <div className="flex flex-row">
                           <svg
-                            viewBox="0 0 1024 1024"
+                            viewBox="0 0 24 24"
                             fill="currentColor"
                             height="2em"
                             width="2em"
                           >
-                            <path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 000 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z" />
+                            <path d="M12 14c2.206 0 4-1.794 4-4s-1.794-4-4-4-4 1.794-4 4 1.794 4 4 4zm0-6c1.103 0 2 .897 2 2s-.897 2-2 2-2-.897-2-2 .897-2 2-2z" />
+                            <path d="M11.42 21.814a.998.998 0 001.16 0C12.884 21.599 20.029 16.44 20 10c0-4.411-3.589-8-8-8S4 5.589 4 9.995c-.029 6.445 7.116 11.604 7.42 11.819zM12 4c3.309 0 6 2.691 6 6.005.021 4.438-4.388 8.423-6 9.73-1.611-1.308-6.021-5.294-6-9.735 0-3.309 2.691-6 6-6z" />
                           </svg>
-                        </a>
-                      </div>
-                    </td>
-                  </tr>
-                )})
-                }
+                          <a
+                            href="#"
+                            className="font-medium text-gray-600 hover:underline"
+                          >
+                            <svg
+                              viewBox="0 0 1024 1024"
+                              fill="currentColor"
+                              height="2em"
+                              width="2em"
+                            >
+                              <path d="M724 218.3V141c0-6.7-7.7-10.4-12.9-6.3L260.3 486.8a31.86 31.86 0 000 50.3l450.8 352.1c5.3 4.1 12.9.4 12.9-6.3v-77.3c0-4.9-2.3-9.6-6.1-12.6l-360-281 360-281.1c3.8-3 6.1-7.7 6.1-12.6z" />
+                            </svg>
+                          </a>
+                        </div>
+                      </td>
+                    </tr>
+                  );
+                })}
               </tbody>
             </table>
           </div>
